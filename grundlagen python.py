d63c82289ade1eee05bcaa12601bf39aa2b05976
StepMaster <171wita26@o356.htl-leoben.at>
2018-04-23 14:20:46 +0200

    grundlagen python.py

--- grundlagen python.py
+++ grundlagen python.py
@@ -1,1 +1,19 @@
 # grundlagen python.py
+
+# Kommentare erfoölgen mit hashtag
+
+# Ausgabe von Daten
+print("Hallo World")
+
+# Variablen definieren (kann ohne Typ erfolgen)
+heimat = "Erde"
+
+# Mehrere Variablen werden mit , getrennt
+print (heimat, "an World: ", "Hallo")
+
+# Eingabe / liest Text(!) von der Konsole ein
+wer = input("Und wer bist du?")
+
+# und gibt den Text wieder aus
+print ("Hallo", wer)
+
