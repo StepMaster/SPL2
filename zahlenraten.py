ac1ddcd677cd1db4229b07ff8cc44a7dee8611b9
StepMaster <171wita26@o356.htl-leoben.at>
2018-04-23 16:21:26 +0200

    zahlenraten.py

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,21 +1,29 @@
 # zahlenraten.py
 max = 200 #input ("Bitte geben Sie die maximale Zahl ein: ")
 Mysterie = input ("Bitte gib deine Zahl ein: ")
+Mysterie=int(Mysterie)
 gewonnen = False
 counter =0
 
-zahl == max:2
+zahl = max/2
+zahl= int(zahl)
 
 while(gewonnen == False):
     counter += 1
 
-    if(zahl==Mysterie):
+    if(zahl == Mysterie):
         gewonnen == True
-    elif(zahl<Mysterie):
+
+    elif(zahl < Mysterie):
+
         print("Zahl ist kleiner!")
-        zahl =(zahl-(zahl:2))
-    elif(zahl<Mysterie):
+        zahl=int(zahl-(zahl/2))
+        print(zahl)
+
+    elif(zahl > Mysterie):
+
         print("Zahl ist groesser!")
-        zahl=(zahl+(zahl:2))
+        zahl = int(zahl+(zahl/2))
+        print(zahl)
 
 
